25f18391e1fec7c739003c7cbb4acf8cf4bfda54
azir999 <muhammadazir188@gmail.com>
2025-02-21 16:03:54 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+  <html>
+    <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      @vite('resources/css/app.css')
+    </head>
+    <body class="bg-black">
+    <header>
+    <nav>
+      <div class="bg-black p-1">
+        <div>
+            <h1 class="font-sans text-5xl font-bold text-left text-red-500 ml-20 italic">
+            Meat <text class="text-3xl font-bold text-left text-white -ml-2">
+            FLIX.id 
+            </text>
+            </h1>
+        </div>
+      <div class=""><button type=button class=" bg-red-500 hover:bg-red-800 hover:text-black text-2xl font-bold p-1 text-white rounded-sm absolute top-2.5 right-5">Login as an admin</button></div>
+    </div>
+  </nav> 
+</header>
+    <div class="bg-gradient-to-r from-red-500 via-purple-600 to-violet-500 p-40 text-white">
+          <div class="text-center text-5xl font-bold font-sans italic">CHANGE YOUR LIFE WITH G.O.A.T MOVIES</div>
+    <div><p class="text-3xl font-sans text-center font-bold not-italic">watch some meat movies with me, hell ya!!!</br>harga mulai dari Rp.14.000</p></div>
+    </div>
+      <div class="text-left absolute left-5 mt-5 ml-5 font-sans font-bold text-3xl">
+            <h3 class="ml-10 text-white">G.O.A.T Movies & Series ></h3>
+            <div class="flex flex-row mt-10">
+            <img src="/images/lighthouse.png" class=" ml-8 w-64 h-96 rounded-lg border-2 border-white hover:brightness-150"><p class="ordinal text-center text-5xl font-bold font-sans italic text-yellow-300 absolute top-20 left-10">1st</p></img>
+            <img src="/images/cruella.png" class=" ml-8 w-64 h-96 rounded-lg border-2 border-white hover:brightness-150"></img>
+            <img src="/images/poorthings.png" class=" ml-8 w-64 h-96 rounded-lg border-2 border-white hover:brightness-150"></img>
+            <img src="/images/civilwar.png" class=" ml-8 w-64 h-96 rounded-lg border-2 border-white hover:brightness-150"></img>
+            <img src="/images/gravityfalls.png" class=" ml-8 w-64 h-96 rounded-lg border-2 border-white hover:brightness-150"></img>
+      </div>
+      </br>
+      </div>
+      <!-- <footer class="text-white text-center bg-black p-5 fixed bottom-0 w-full">
+      <h1>copyright 2025 meatflix.id</h1>
+      </footer> -->
+    </body>
+</html
